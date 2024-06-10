aa26714a900201dae6875970988cc98bcf6b4703
skolton09 <skolton09@gmail.com>
2024-06-10 06:59:06 -0700

    Initial

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var indexRouter = require("./routes/index.js");
+var secondRouter = require("./routes/second.js");
+
+var app = express();
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+app.use('/second', secondRouter);
+
+app.listen(3000, () => {
+    console.log('Running on port 3000')
+});
+
