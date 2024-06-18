a1ab27ce3dd5988ac652d566347b03a8043bd917
skolton09 <skolton09@gmail.com>
2024-06-18 10:54:37 -0700

    Try instrumentation file

--- app.js
+++ app.js
@@ -1,29 +1,3 @@
-import { AwsRum } from 'aws-rum-web';
-
-try {
-  const config = {
-    sessionSampleRate: 1,
-    identityPoolId: "us-east-1:1f4aa60b-6415-41ce-bb6b-1bb2aac9e2b0",
-    endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
-    telemetries: ["performance","http"],
-    allowCookies: true,
-    enableXRay: true
-  };
-
-  const APPLICATION_ID = '0d6c7d54-c31a-45b6-be39-118455091554';
-  const APPLICATION_VERSION = '1.0.0';
-  const APPLICATION_REGION = 'us-east-1';
-
-  const awsRum = new AwsRum(
-    APPLICATION_ID,
-    APPLICATION_VERSION,
-    APPLICATION_REGION,
-    config
-  );
-} catch (error) {
-  // Ignore errors thrown during CloudWatch RUM web client initialization
-}
-
 import express from "express";
 import https from "https";
 
