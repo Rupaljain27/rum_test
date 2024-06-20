9d2a4de6dcebeef421dc112a227f8b69ee603dd1
skolton09 <skolton09@gmail.com>
2024-06-20 00:38:48 -0700

    Change link

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import https from "https";
+import path from "path";
+import { fileURLToPath } from 'url';
 
 import indexRouter  from "./routes/index.js";
 import secondRouter from "./routes/second.js";
@@ -11,8 +13,12 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
-app.use("/aws-rum-web", express.static("node_modules/aws-rum-web"));
+app.use(express.static("node_modules"));
 
+// const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+// const __dirname = path.dirname(__filename); // get the name of the directory
+
+// app.use("/aws-rum-web", express.static(path.join(__dirname, "node_modules/aws-rum-web/")));
 app.use('/', indexRouter);
 app.use('/second', secondRouter);
 
