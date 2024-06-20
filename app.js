e97207c193a73e3a4c1331dab12b52cb8e57c1da
skolton09 <skolton09@gmail.com>
2024-06-20 00:28:09 -0700

    Added link to redirtect aws-rum-web

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
+app.use("/aws-rum-web", express.static("node_modules/aws-rum-web"));
 
 app.use('/', indexRouter);
 app.use('/second', secondRouter);
