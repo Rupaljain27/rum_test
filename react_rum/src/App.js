86b81684091d6a88da9859fc329cdf70eaaec9c1
Rupal Jain <jainrupal@arizona.edu>
2024-06-30 16:52:50 -0700

    Testing react

--- react_rum/src/App.js
+++ react_rum/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate  } from "react-router-dom";
 
 import { AwsRum } from 'aws-rum-web';
 import { useEffect } from 'react';
